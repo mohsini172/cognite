a2b5fa192e8caee6b293ca56b05378d954f7adf6
Mohsin Ammar <mohsini172@gmail.com>
2022-09-23 23:11:27 +0200

    Packages update

--- server/db/mongoose.ts
+++ server/db/mongoose.ts
@@ -2,18 +2,20 @@ import mongoose from 'mongoose';
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
+import { MongoMemoryServer } from 'mongodb-memory-server';
 
-// var options = {
-// 	keepAlive: 300000,
-// 	connectTimeoutMS: 30000,
-//     user: "admin",
-// 	pass: "Pa$$w0rd!",
-// };
-mongoose.connect("mongodb://admin:Pa$$w0rd!@ds261247.mlab.com:61247/congnite");
+// This will create an new instance of "MongoMemoryServer" and automatically start it
+MongoMemoryServer.create().then(mongod => {
+  const uri = mongod.getUri();
+  mongoose.connect(
+    uri
+  );
+});
+
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
-    console.log("mongodb connected");
+  console.log("mongodb connected");
 });
 
 module.exports = mongoose;
